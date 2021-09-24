25caca4ec27f8382275947c92176f05f80447ce3
Anthony Nguyen <nguyean6@oregonstate.edu>
2021-09-23 19:42:30 -0700

    lab1

--- lab1/lab1_code.c
+++ lab1/lab1_code.c
@@ -7,7 +7,7 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
-#define SWITCH_COUNT 8
+#define MAX_BYTE_DEBOUNCE 8 // numbers of bytes for the debounce
 
 //*******************************************************************************
 //                            debounce_switch
@@ -16,64 +16,59 @@
 // Function returns a 1 only once per debounced button push so a debounce and toggle
 // function can be implemented at the same time.  Expects active low pushbutton on
 // Port D bit zero.  Debounce time is determined by external loop delay times 12.
+//
+// Passed in the button we want to check
+//
 //*******************************************************************************
-int8_t debounce_switch(int button) {
-  static uint16_t state[SWITCH_COUNT]; //holds present state
-  
+int8_t debounce_switch(int button)
+{
+  static uint16_t state[MAX_BYTE_DEBOUNCE]; //holds present state
+
   // bit_is_clear: test whether but but in IO register sfr is clear. This will return non zero
   // if the but is clear, and 0 if the bit is set
   // handling multiple inputs
   // https://www.avrfreaks.net/sites/default/files/debouncing.pdf
   state[button] = (state[button] << 1) | (!bit_is_clear(PIND, button)) | 0xE000; // when the second button is pressed
+
+  // debuging to see how state debouncing works
   // PORTB = state[button];
   // _delay_ms(1000);
+
+  // once the certain switch reaches the value it will return 1
   if (state[button] == 0xF000)
     return 1;
 
-  
-
   return 0;
 }
-
-
 
 //*******************************************************************************
 // Check switch S0.  When found low for 12 passes of "debounce_switch(), increment
 // PORTB.  This will make an incrementing count on the port B LEDS.
 //*******************************************************************************
-int main() {
+int main(){
   DDRB = 0xFF; //set port B to all outputs
-  while (1) { //do forever
+  while (1){ //do forever
     // int result = debounce_switch();
     // if (result == 1) {
     //   PORTB++;
     // }             //if switch true for 12 passes, increment port B
-    // else if (result == 2) {
-    //   PORTB--;
-    // }
 
-    // if (!bit_is_clear(PIND, 0))
-    //   PORTB++;
-    // if (!bit_is_clear(PIND, 1))
-    //   PORTB--;
+    // check if S1 was pressed
+    if (debounce_switch(0))
+      PORTB++; // if it was pressed then increase value
+    
 
-    if(debounce_switch(0)){
-      PORTB++;
-    }
-    if (debounce_switch(1)){
-      PORTB--;
-    }
+    // Now...modify the code so that the S1 push button increments the count by one and
+    // S2 decrements the count by one. The count will "roll over after 0xFF to 0x00 and
+    // will roll under to 0xFF from 0x00. When S1 and S0 are pushed simultaneously, the
+    // count will not change.
 
-
+    // check if S2 was pressed
+    if (debounce_switch(1))
+      PORTB--; // if it was pressed then decrease value
+    
 
     _delay_ms(2); //keep in loop to debounce 24ms
   }               //while
-
-  // Now...modify the code so that the S1 push button increments the count by one and
-  // S2 decrements the count by one. The count will "roll over after 0xFF to 0x00 and
-  // will roll under to 0xFF from 0x00. When S1 and S0 are pushed simultaneously, the
-  // count will not change.
-
-
 
 } //main
