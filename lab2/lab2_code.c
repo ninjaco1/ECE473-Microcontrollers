e023d646f949396393795e19e2a53072480a06ea
Anthony Nguyen <nguyean6@oregonstate.edu>
2021-10-06 16:49:32 -0700

    basic logic

--- lab2/lab2_code.c
+++ lab2/lab2_code.c
@@ -2,6 +2,9 @@
 // Antony Nguyen
 // 10.4.2021
 
+#include <avr/io.h>
+#include <util/delay.h>
+
 //  HARDWARE SETUP:
 //  PORTA is connected to the segments of the LED display. and to the pushbuttons.
 //  PORTA.0 corresponds to segment a, PORTA.1 corresponds to segement b, etc.
@@ -11,49 +14,59 @@
 #define F_CPU 16000000 // cpu speed in hertz
 #define TRUE 1
 #define FALSE 0
-#define MAX_BYTE_DEBOUNCE 8 // numbers of bytes for the debounce
+#define MAX_BIT_DEBOUNCE 8 // numbers of bytes for the debounce
 
-// segs to turn on for LED
-#define ONE 0b0000 0110   // B, C
-#define TWO 0b0101 1011   // A, B, D, E, G
-#define THREE 0b0100 1111 // A, B, C, D, G
-#define FOUR 0b0110 0110  // B, C, F, G
-#define FIVE 0b0110 1101  // A, C, D, F, G
-#define SIX 0b0111 1101   // A, C, D, E, F, G
-#define SEVEN 0b0000 0111 // A, B, C
-#define EIGHT 0b0111 1111 // A, B, C, D, E, F, G
-#define NINE 0b0110 0111  // A, B, C, F, G
-
+// segs to turn on for LED, negate everything
+#define ZERO  0b00111111   // A, B, C, D, E, F
+#define ONE   0b00000110   // B, C
+#define TWO   0b01011011   // A, B, D, E, G
+#define THREE 0b01001111   // A, B, C, D, G
+#define FOUR  0b01100110   // B, C, F, G
+#define FIVE  0b01101101   // A, C, D, F, G
+#define SIX   0b01111101   // A, C, D, E, F, G
+#define SEVEN 0b00000111   // A, B, C
+#define EIGHT 0b01111111   // A, B, C, D, E, F, G
+#define NINE  0b01100111   // A, B, C, F, G
+#define BLANK 0x00
+#define COLON 0b00000011   // A, B
 // Port B decoder, remember to not all the digits
-#define DIGIT1 (1 << PB6) | (0 << PB5) | (0 << PB4)
+#define DIGIT1 PORTB  & ((1 << PB6) & (0 << PB5) & (0 << PB4))
 #define DIGIT2 (0 << PB6) | (1 << PB5) | (0 << PB4)
 #define DIGIT3 (0 << PB6) | (0 << PB5) | (1 << PB4)
 #define DIGIT4 (0 << PB6) | (0 << PB5) | (0 << PB4)
 #define TRI_BUFFER (1 << PB6) | (1 << PB5) | (1 << PB4)
 
-#define CLR_SEGMENTS 0x00 
-
-
-
-#include <avr/io.h>
-#include <util/delay.h>
 
 //holds data to be sent to the segments. logic zero turns segment on
 uint8_t segment_data[5]
 
 //decimal to 7-segment LED display encodings, logic "0" turns on segment
 uint8_t dec_to_7seg[12]
+dec_to_7seg[0] = ~ZERO;
+dec_to_7seg[1] = ~ONE;
+dec_to_7seg[2] = ~TWO;
+dec_to_7seg[3] = ~THREE;
+dec_to_7seg[4] = ~FOUR;
+dec_to_7seg[5] = ~FIVE;
+dec_to_7seg[6] = ~SIX;
+dec_to_7seg[7] = ~SEVEN;
+dec_to_7seg[8] = ~EIGHT;
+dec_to_7seg[9] = ~NINE;
+dec_to_7seg[10] = ~COLON;
+dec_to_7seg[11] = ~BLANK;
 
 uint8_t chk_buttons(uint8_t button); // check what button is being pressed
 void segsum(uint16_t sum);
 void setDigit(int);
+void clearDecoder();
 
 uint8_t main()
 {
-    DDRB = 0xE0;         //set port B bits 4-7 B as outputs
+    DDRB = 0xF0;         //set port B bits 4-7 B as outputs
    
     int current_num = 0; // the number that will be on the display
 
+    // initially display all 0s on the display
     // Steps in what happens
     // person press a button, port A, phase 6
     // set that increment digit to a value, increment variable
@@ -74,13 +87,12 @@ uint8_t main()
 
         // for loop for each phase of the digit
         for (j = 0; j < 12; j++){ // for the debounce
-
             for (i = 0; i < 8; i++){
                 // checking what button is being pressed
-                inc = 0; // increment value
+                inc = 0; // increment initalize to 0 first
                 if(chk_button(i)){
-                    
-
+                    inc = 1 << i; 
+                    //
 
 
                 }
@@ -133,7 +145,7 @@ uint8_t main()
 //
 uint8_t chk_buttons(uint8_t button){
 
-    static uint16_t state[MAX_BYTE_DEBOUNCE]; //holds present state
+    static uint16_t state[MAX_BIT_DEBOUNCE]; //holds present state
 
     // bit_is_clear: test whether but but in IO register sfr is clear. This will return non zero
     // if the but is clear, and 0 if the bit is set
@@ -158,43 +170,70 @@ uint8_t chk_buttons(uint8_t button){
 //BCD segment code in the array segment_data for display.
 //array is loaded at exit as:  |digit3|digit2|colon|digit1|digit0|
 void segsum(uint16_t sum)
-{
-    //determine how many digits there are
+{   
+    // sum is the total count, place each digit into segment_data[5]
+    // determine how many digits there are
     //break up decimal sum into 4 digit-segments
     //blank out leading zero digits
     //now move data to right place for misplaced colon position
+
+    segment_data[2] = 0; // doesn't turn on the colon
+    int i, remainder, mod, digit;
+    mod = 1000;
+    for (i = 4; i >= 0; i--){
+        if(i == 2)
+            continue;
+        digit = sum / mod;
+        remainder = sum % mod;
+        segment_data[4-i] = digit;
+        sum = remainder;
+        mod /= 10; // divide the mod to 10
+    }
+
+
+
 } //segment_sum
-//***********************************************************************************
+
+
+
+
+/*************************************************************************************/
+//                                  clearDecoder
+// Clears the bits for the decoder. Since the decoder usings bits 4-6, they will be 
+// zero afterwards.
+/*************************************************************************************/
+
+void clearDecoder(){
+    PORTB &= (0 << PB6) | (0 << PB5) | (0 << PB4);
+}
 
 /***************************************************************/
-// setDigit function
+//                      setDigit function
 // it will choose its given digit and set that number for it
 //
 /***************************************************************/
 
-void setDigit(int digit)
-{
+void setDigit(int digit){
 
     switch (digit)
     {
 
-    case 0:
-        PORTB &= ~((1 << PB6) | (1 << PB5) | (1 << PB4)); // turning on digit 4, clearing all its bits
-
-        break;
-    case 1:
-        PORT
+        case 0:
+            
 
             break;
-    case 2:
-        break;
-    case 3:
-        break;
-    case 4:
-        break;
-    case 5:
-        break;
-    default:
-        break;
+        case 1:
+
+                break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        default:
+            break;
     }
 }
