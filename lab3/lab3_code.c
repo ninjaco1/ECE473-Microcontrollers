7b086cacadb7f858c32a93b65a2764442fd2533e
Anthony Nguyen <nguyean6@oregonstate.edu>
2021-10-24 23:07:12 -0700

    need to read input

--- lab3/lab3_code.c
+++ lab3/lab3_code.c
@@ -67,6 +67,10 @@ uint8_t dec_to_7seg[12];
 // Decoder 3 to 8
 uint8_t decoder[8];
 
+// current number on the display
+uint16_t current_num = 0;
+
+
 // prototypes
 int8_t chk_buttons(int button); // check what button is being pressed
 void segsum(uint16_t sum);
@@ -77,63 +81,31 @@ void set_decoder();
 // timers fucntions
 void spi_init(void);
 void tcnt0_init(void);
-// ISR(TIMER0_OVF_vect);
-// decoder
+ISR(TIMER0_OVF_vect);
+uint8_t read_write_spi(void);
+// encoder
 int8_t encoder_chk(uint8_t encoder_var);
+void barGraph();
 
 int main()
 {
     DDRB = 0xF0; // set port B bits 4-7 B as outputs, decoder
-    DDRE = 0x20; // set E6 to output
+    DDRE = 0x40; // set E6 to output
     DDRD = 0xB0; // slave select pins
 
     
-    PORTE |= 0 << PORTE6; // SH/LD held low so it doesn't read from serial in but from encoder
+    
 
-    uint16_t current_num = 0; // the number that will be on the display
+    // current_num = 0; // the number that will be on the display
     tcnt0_init();  //initalize counter timer zero
     spi_init();    //initalize SPI port
     sei();         //enable interrupts before entering loop
     set_dec_to_7seg(); // set values for dec_to_7seg array
     set_decoder(); // set values for the decoder array
 
-    
 
     while (1)
     {
-
-        uint16_t i, j, inc;
-        
-        
-        //enable tristate buffer for pushbutton switches
-        PORTB = TRI_BUFFER;
-        //insert loop demake lay for debounce
-        for (j = 0; j < 12; j++)
-        { // for the debounce
-            for (i = 0; i < 8; i++)
-            {
-                //make PORTA an input port with pullups
-                DDRA = 0x00;  // set port A as inputs
-                PORTA = 0xFF; // set port A as pull ups
-                
-                inc = 0; // increment initalize to 0 first
-
-
-                // checking what button is being pressed
-                // if (chk_buttons(i))
-                // {
-                //     inc = 1 << i;
-                //     current_num = current_num + inc;
-                // }
-                
-                if (chk_buttons(1)){ // S1, + or - 2
-                    inc = 1 << 1;
-                }
-                if (chk_buttons(2)){ // S2, + or - 4
-                    inc = 1 << 2;
-                }
-            }
-        }
 
         if (current_num > 1023)
             current_num -= 1024;
@@ -260,9 +232,11 @@ void setDigit()
 {
     DDRA = 0xFF; // setting PORT A as an output
     int i;
+    uint8_t temp_portb; // holds old value of port b
     for (i = 0; i < 5; i++)
     { // looping through the segment data and assigning the port the right values.
-        PORTB = decoder[i]; // enable the right digit to turn on
+        temp_portb = PORTB & 0x0f; // taket he 4 LSB bits 
+        PORTB = decoder[i] | temp_portb; // enable the right digit to turn on
         PORTA = dec_to_7seg[segment_data[i]]; // turn on the right segments
         _delay_ms(0.5);
     }
@@ -304,20 +278,158 @@ void tcnt0_init(void){
 //(1/32768)*256*64 = 500mS
 /*************************************************************************/
 ISR(TIMER0_OVF_vect){
-    static uint8_t count_7ms = 0;        //holds 7ms tick count in binary
-    static uint8_t display_count = 0x01; //holds count for display 
+    // static uint8_t count_7ms = 0;        //holds 7ms tick count in binary
+    // static uint8_t display_count = 0x01; //holds count for display 
 
-    count_7ms++;                //increment count every 7.8125 ms 
-    if ((count_7ms % 64)==0){ //?? interrupts equals one half second 
-        SPDR = display_count;               //send to display 
-        while (!(TIFR & (1 << TOV0))){}               //wait till data sent out (while loop)
-        PORTB |= (1 << PORTB0);          //HC595 output reg - rising edge...
-        PORTB &= (0 << PORTB0);          //and falling edge
-        display_count = display_count << 1; //shift display bit for next time 
+    // count_7ms++;                            //increment count every 7.8125 ms 
+    // if ((count_7ms % 64)==0){               //?? interrupts equals one half second 
+    //     SPDR = display_count;               //send to display 
+    //     while (!(TIFR & (1 << TOV0))){}               //wait till data sent out (while loop)
+    //     PORTB |= (1 << PORTB0);          //HC595 output reg - rising edge...
+    //     PORTB &= (0 << PORTB0);          //and falling edge
+    //     display_count = display_count << 1; //shift display bit for next time 
+    // }
+    // if (display_count == 0x80){
+    //     display_count= 1;
+    // } //back to 1st positon
+
+
+
+    // check for encoder
+    static uint8_t new_A = 0, old_A = 0, new_B = 0, old_B = 0, serial_out = 0, incrementFlag = 1, oldincflag;
+    uint8_t return_val, count;
+
+    
+    // getting the data from serial out
+    PORTD &= 0 << PORTD3; // turn on slave select the encoder
+    PORTD &= 0 << PORTD2; // turn off slave select for bar graph
+    PORTE |= 0 << PORTE6; // SH/LD held low so it doesn't read from serial in but from encoder
+
+    // clk pulse
+    PORTB |= 1 << PORTB1;
+    PORTB &= 0 << PORTB0;
+    serial_out = read_write_spi(); // read 
+    
+
+    // encoder 1
+    new_A = serial_out & 0x03; // buttom 2 bits
+    
+    // encoder 2
+    new_B = (serial_out & 0x0C) >> 2; // the next two then right shift the bits so they match new_A
+    oldincflag = incrementFlag;
+    return_val = -1; // default return value, no change
+    if ((new_A != old_A) || (new_B != old_B)){ // if change occured
+        if((new_A == 0) && (new_B == 0)){
+            if (old_A == 0){
+                // count++;
+                incrementFlag = 1;
+            }
+            else{
+                // count--;
+                incrementFlag = 0;
+            }
+        }
+        else if ((new_A == 0) && (new_B == 1)){
+            if (old_A == 0){
+                // count++;
+                incrementFlag = 1;
+            }
+            else{
+                // count--;
+                incrementFlag = 0;
+
+            }
+        }
+        else if ((new_A == 1) && (new_B == 1)){ // detent position
+            if (old_A == 0){ // one direction 
+                // if (count == 3){
+                //     return_val = 0;
+                // }
+                if (incrementFlag == 1)
+                    return_val = 0;
+            }
+            else{ // or the other direction
+                // if (count == -3){
+                //     return_val = 1;
+                // }
+                if (incrementFlag == 0)
+                    return_val = 1;
+            }
+            // count = 0; // count is always reset in detent position
+        }
+        else if ((new_A == 1) && (new_B == 0)){
+            if (old_A == 1){
+                // count++;
+                incrementFlag = 1;
+            }
+            else{ 
+                // count--;
+                incrementFlag = 0;
+            }
+        }
+
+        old_A = new_A; // save what are now old values
+        old_B = new_B;
+
+    } // if changed occured
+    // if return value is still -1 then nothing happen
+    // if (return_val == -1){
+    //     current_num += 1;
+    //     return;
+    // }
+
+    
+
+    // check for push buttons
+    uint16_t i, j, inc;
+    
+    //enable tristate buffer for pushbutton switches
+    PORTB |= TRI_BUFFER;
+    //insert loop demake lay for debounce
+    for (j = 0; j < 12; j++)
+    { // for the debounce
+        // for (i = 0; i < 8; i++)
+        // {
+            //make PORTA an input port with pullups
+            DDRA = 0x00;  // set port A as inputs
+            PORTA = 0xFF; // set port A as pull ups
+            
+            inc = 0; // increment initalize to 0 first
+
+
+            // checking what button is being pressed
+            // if (chk_buttons(i))
+            // {
+            //     inc = 1 << i;
+            //     current_num = current_num + inc;
+            // }
+            
+            if (chk_buttons(1)){ // S1, + or - 2
+                inc = 1 << 1;
+                // check if its increment or decrement
+                // current_num += inc;
+                if (return_val == -1){
+                    
+                }
+                if (incrementFlag == 1)
+                    current_num += inc;
+                else if(incrementFlag == 0)
+                    current_num -= inc;
+            }
+            if (chk_buttons(2)){ // S2, + or - 4
+                inc = 1 << 2;
+                // current_num += inc;
+                if (incrementFlag == 1)
+                    current_num += inc;
+                else if (incrementFlag == 0)
+                    current_num -= inc;
+            }
+        // }
     }
-    if (display_count == 0x80){
-        display_count= 1;
-    } //back to 1st positon
+
+
+
+
 }
 
 /*************************************************************************/
@@ -345,4 +457,16 @@ int8_t encoder_chk(uint8_t encoder_var){
         return (b_pin) ? 1 : 0;
     else
         return -1;
+}
+
+
+/*************************************************************************/
+//                            read_write_spi
+//
+/*************************************************************************/
+uint8_t read_write_spi(uint8 dataout){
+    // load data into spi buffer
+    SPDR = 0;
+    while (bit_is_clear(SPSR,SPIF)){}
+    return (SPDR);
 }
